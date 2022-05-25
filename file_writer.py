3141526415eb874cbe088adcbc8a79b7adfc914b
lixavi <121849896+lixavi@users.noreply.github.com>
2022-05-25 23:12:00 +0530

    updated

---
+++ file_writer.py
@@ -0,0 +1,19 @@
+import os
+
+def write_files(code_files, directory):
+    """
+    Write optimized code to files in the specified directory.
+
+    Args:
+    - code_files (dict): A dictionary containing file names as keys and optimized code as values.
+    - directory (str): The directory path where the files will be written.
+    """
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    for file_name, code_content in code_files.items():
+        file_path = os.path.join(directory, file_name)
+        with open(file_path, 'w') as file:
+            file.write(code_content)
+
+        print(f"Optimized code written to '{file_path}'")
