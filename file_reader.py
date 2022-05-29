b27f1be0194a0ddf29b96e5a8e0652daf62973cf
lixavi <121849896+lixavi@users.noreply.github.com>
2022-05-29 23:13:18 +0530

    updated

--- file_reader.py
+++ file_reader.py
@@ -7,3 +7,6 @@ def read_files(directory):
     Args:
     - directory (str): The directory path from which files will be read.
 
+    Returns:
+    - code_files (dict): A dictionary containing file names as keys and code contents as values.
+    """
